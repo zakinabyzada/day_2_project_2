2a908dd331af4c43752193c0620f1dc632148cd8
zaki <zaki@localhost>
2023-04-09 23:30:42 -0700

    project 2 first stash

---
+++ src/day_2_project_2/day_2_class.java
@@ -0,0 +1,10 @@
+package day_2_project_2;
+
+public class day_2_class {
+	public static void main(String [] args	) {
+		System.out.println("hog rider");
+	}
+	public static void Resturant () {
+		System.out.println("MENU");
+	}
+}
